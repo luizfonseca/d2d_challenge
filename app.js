3734040cc986ca097b84349fe117a026fb0f5f82
Luiz Fonseca <eu@luiz.cc>
2017-12-01 16:14:42 +0100

    Add CORS allow-policy to the API server

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ mongoose.connect(mongoURI, { useMongoClient: true });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const routes = require('./api/routes/vehicleRoutes'); //importing route
 routes(app);
 
